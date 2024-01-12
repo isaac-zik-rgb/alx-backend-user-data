11fb0b6df205e21f1d6edf3bfdf40efff538e1b6
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-12 10:52:56 +0100

    Fix indentation in filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,11 @@
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str,
-                    message: str, separator: str) -> str:
-        """returns the log message obfuscated"""
-        for i in fields:
-            message = re.sub(i + "=.*?" + separator,
-                            i + "=" + redaction + separator, message)
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for i in fields:
+        message = re.sub(i + "=.*?" + separator,
+                         i + "=" + redaction + separator, message)
         return message
