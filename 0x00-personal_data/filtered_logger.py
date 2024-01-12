1556cddb748a30ada9c45edf7f3573349ce4494b
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-12 10:56:01 +0100

    Fix bug in filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,5 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for i in fields:
-        message = re.sub(i + "=.*?" + separator,
-                         i + "=" + redaction + separator, message)
-        return message
+        return re.sub(i + "=.*?" + separator,
+                      i + "=" + redaction + separator, message)
