7771bc59cc72ea73d6fe43578edb8d344dca14c3
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-17 00:49:17 +0100

    Add current_user method to BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,3 +66,24 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        b64_auth_header = self.extract_base64_authorization_header(auth_header)
+        if b64_auth_header is None:
+            return None
+        decoded_b64_auth_header = self.decode_base64_authorization_header(
+            b64_auth_header)
+        if decoded_b64_auth_header is None:
+            return None
+        user_credentials = self.extract_user_credentials(
+            decoded_b64_auth_header)
+        if user_credentials is None:
+            return None
+        user_email, user_pwd = user_credentials
+        user = self.user_object_from_credentials(user_email, user_pwd)
+        return user
