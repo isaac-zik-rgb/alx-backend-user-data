4b476c8cbc2e92563e22fcf7a615ab7c11ad17e2
isaac okechukwu chukwuma <isaacokechukwu200021@gmail.com>
2024-01-16 13:06:38 +0100

    wrote theauth class template

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""A Basic Authentication class"""
+
+from flask import request
+
+
+class Auth:
+    """A Basic Auth Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Check if authentication is required for a given path.
+        This function determines whether authentication is
+        required for a specific
+        path, taking into account a list of excluded paths
+        where authentication is not enforced.
+        Parameters:
+             - path (str): The path for which authentication
+             requirement is being checked.
+             - excluded_paths (List[str]): A list of paths where
+             authentication is not enforced.
+             Returns:
+             bool: True if authentication is required for the
+             given path, False otherwise.
+        Example:
+             ```
+             auth_required = require_auth('/secured/resource',
+             excluded_paths=['/public', '/unsecured'])
+             if auth_required:
+             # Perform authentication logic
+             else:
+             # Authentication is not required for the given path
+             ```
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ an authorization header method that returns the request object
+        """
+        return request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """This function returns the current user that is authorized
+        """
+        return request
