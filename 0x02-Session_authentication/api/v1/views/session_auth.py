5b58d06477b2f633f7165c7a5ec96004f37e3d9b
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-18 13:00:28 +0100

    Fix authentication bug in session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -15,14 +15,15 @@ def auth_session() -> str:
         return jsonify({"error": "email missing"}), 400
     if not password:
         return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': email})
-    if not user:
+    users = User.search({'email': email})
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
-    for user in user:
+    for user in users:
         if user.is_valid_password(password):
             from api.v1.app import auth
             session_id = auth.create_session(user.id)
             json = jsonify(user.to_json())
             json.set_cookie(getenv('SESSION_NAME'), session_id)
             return json
-        return jsonify({"error": "wrong password"}), 40
+        else:
+            return jsonify({"error": "wrong password"}), 401
