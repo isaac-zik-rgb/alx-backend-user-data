a2f28c7424641469a9d17b0127242861beceb98f
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-18 10:25:04 +0100

    Add session creation functionality

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,19 @@
 """Session authentication"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class"""
+    user_id_by_session_id = {}
 
-    pass
+    def create_session(self, user_id: str = None) -> str:
+        """Create session"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        if session_id is None:
+            return None
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
