388af80bc08b3b4ad8f31a0c105730460566361f
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-18 12:26:42 +0100

    Remove unused code related to session authentication

--- 0x02-Session_authentication/api/v1/__init__.py
+++ 0x02-Session_authentication/api/v1/__init__.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 """add session auth"""
 from api.v1.views.session_auth import *
-
-
-app_views.route("/auth_session/logout", methods=['DELETE'], strict_slashes=False)(session_login)
