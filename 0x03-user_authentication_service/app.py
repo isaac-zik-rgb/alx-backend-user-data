096c89e4405b01e55a8ae99c46b77ce1218633f3
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-24 16:00:02 +0100

    7. Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,3 +30,6 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{email}", "message": "user created"})
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
